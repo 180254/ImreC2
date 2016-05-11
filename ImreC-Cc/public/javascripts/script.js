466333583b313bcd2ab0507ab65390de1e0d8471
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-05-12 00:52:09 +0200

    (cc) refactored & customized ajax file uploading

--- ImreC-Cc/public/javascripts/script.js
+++ ImreC-Cc/public/javascripts/script.js
@@ -1,65 +1,101 @@
 'use strict';
 
-// https://www.designedbyaturtle.co.uk/2015/direct-upload-to-s3-using-aws-signature-v4-php/
-$(document).ready(function () {
+$(function () {
 
-    // Assigned to variable for later use.
-    var form = $('.direct-upload');
-    var filesUploaded = [];
+    var initHideAble = function ($elem) {
+        $elem.click(function () {
+            $elem.slideUp('slow');
+        })
+    };
 
-    // Place any uploads within the descending folders
-    // so ['test1', 'test2'] would become /test1/test2/filename
-    var folders = [];
-
-    form.fileupload({
-        url: form.attr('action'),
-        type: form.attr('method'),
+    var $form = $('#s3form');
+    $form.fileupload({
+        url: $form.attr('action'),
+        type: $form.attr('method'),
         dataType: 'json',
+
         add: function (event, data) {
-            console.log(data.files);
-            // Give the file which is being uploaded it's current content-type (It doesn't retain it otherwise)
-            // and give it a unique name (so it won't overwrite anything already on s3).
-            var file = data.files[0];
-            var filename = file.name;
-            form.find('input[name="Content-Type"]').val(file.type);
+            data.files.forEach(function (file) {
+                $form.find('input[name="Content-Type"]').val(file.type);
 
-            var $key = form.find('input[name="key"]');
-            var newKeyVal = $key.val().replace(/^([a-zA-Z0-9]+\/).*/, '$1' + filename);
-            $key.val(newKeyVal);
+                var $key = $form.find('input[name="key"]');
+                var newKeyVal = $key.val().replace(/^([a-zA-Z0-9]+\/).*/, '$1' + file.name);
+                $key.val(newKeyVal);
 
-            // Actually submit to form to S3.
-            data.submit();
+                var $progress = $('<div/>', {
+                    class: 'progress',
+                    'data-mod': file.size,
+                    'data-file': file.name
+                });
 
-            // Show the progress bar
-            // Uses the file size as a unique identifier
-            var bar = $('<div class="progress" data-mod="' + file.size + '"><div class="bar"></div></div>');
-            $('.progress-bar-area').append(bar);
-            bar.slideDown('slow');
+                var $bar = $('<div/>', {
+                    class: 'bar',
+                    'data-file': file.name
+                });
+                $bar.css('width', '0%');
+                $progress.append($bar);
+
+                $('.progress-bar-area').append($progress);
+                $progress.slideDown('slow');
+
+                data.submit();
+            });
         },
+
         progress: function (e, data) {
-            var percent = Math.round((data.loaded / data.total) * 100);
-            $('.progress[data-mod="' + data.files[0].size + '"] .bar').css('width', percent + '%').html(percent + '%');
+            data.files.forEach(function (file) {
+                var percent = Math.round((data.loaded / data.total) * 100);
+                $('.bar[data-file="' + file.name + '"]')
+                    .css('width', percent + '%')
+                    .html(percent + '%');
+            });
+
         },
 
         fail: function (e, data) {
-            $('.progress[data-mod="' + data.files[0].size + '"] .bar').css('width', '100%').addClass('red').html('');
+            data.files.forEach(function (file) {
+                $('.bar[data-file="' + file.name + '"]')
+                    .css('width', '100%')
+                    .addClass('red')
+                    .html('Failed to add ' + file.name);
+
+                initHideAble($('.progress[data-file="' + file.name + '"]'));
+            });
         },
 
-        done: function (event, data) {
-            $('.progress[data-mod="' + data.files[0].size + '"]').slideUp('slow');
+        done: function (event, data, x) {
+            data.files.forEach(function (file) {
+                $('.progress[data-file="' + file.name + '"]').slideUp('slow');
+                var $fileList = $('#file-list');
 
-            // Upload Complete, show information about the upload in a textarea
-            // from here you can do what you want as the file is on S3
-            // e.g. save reference to your server using another ajax call or log it, etc.
-            var original = data.files[0];
-            var s3Result = data.result.documentElement.children;
-            filesUploaded.push({
-                "original_name": original.name,
-                "s3_name": s3Result[2].innerHTML,
-                "size": original.size,
-                "url": s3Result[0].innerHTML
+                if ($fileList.find('a:contains("' + file.name + '")').length !== 0)
+                    return;
+
+                var url = $form.attr('action');
+                var key = $form.find("input[name='key']");
+                var fullUrl = url + '/' + key;
+
+                var $input = $('<input/>', {
+                    type: 'checkbox',
+                    'name': 'files',
+                    value: file.name
+                });
+                var $url = $('<a/>', {
+                    href: encodeURI(fullUrl)
+                }).html(file.name);
+
+                var $tr = $('<tr/>').append(
+                    $('<td/>').append($input)
+                ).append(
+                    $('<td/>').append($('<span/>').html('?'))
+                ).append(
+                    $('<td/>').append($('<span/>').html('?'))
+                ).append(
+                    $('<td/>').append($url)
+                );
+
+                $fileList.append($tr);
             });
-            $('#uploaded').html(JSON.stringify(filesUploaded, null, 2));
         }
     });
 });
