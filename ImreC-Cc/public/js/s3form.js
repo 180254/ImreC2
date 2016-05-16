d0cead6ec003eeca65ec5dbb097c2ef2bee1a81f
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-05-16 14:22:55 +0200

    (cc) limit number of uploaded file at same time (now limit=5) + displaying how many files are in upload queue

--- ImreC-Cc/public/js/s3form.js
+++ ImreC-Cc/public/js/s3form.js
@@ -1,14 +1,41 @@
 'use strict';
 /* global initFileRow */
 
+var LIMIT_UPLOADING_IN_SAME_TIME = 5;
+
 var initHideAble = function ($elem) {
     $elem.click(function () {
         $elem.slideUp('slow');
     })
 };
 
+// credits: http://stackoverflow.com/a/14811679
+var waitFor = function (test, expectedValue, msec, count, source, callback) {
+    //noinspection LoopStatementThatDoesntLoopJS
+    while (test() !== expectedValue) {
+        count++;
+        setTimeout(function () {
+            waitFor(test, expectedValue, msec, count, source, callback);
+        }, msec);
+        return;
+    }
+    // Condition finally met. callback() can be executed.
+    // console.log(source + ': ' + test() + ', expected: ' + expectedValue + ', ' + count + ' loops.');
+    callback();
+};
+
+var updateUploadQueue = function (uploadQueue) {
+    $('#upload-queue').html(uploadQueue);
+};
+
 $(function () {
     var $form = $('#s3form');
+    var uploading = 0;
+    var uploadQueue = 0;
+
+    var _isBusy = function () {
+        return uploading >= LIMIT_UPLOADING_IN_SAME_TIME;
+    };
 
     //noinspection JSUnusedGlobalSymbols
     $form.fileupload({
@@ -17,30 +44,35 @@ $(function () {
         dataType: 'json',
 
         add: function (event, data) {
+            updateUploadQueue(uploadQueue += data.files.length);
+
             data.files.forEach(function (file) {
-                $form.find('input[name="Content-Type"]').val(file.type);
+                waitFor(_isBusy, false, 300, 0, file.name, function () {
+                    uploading++;
+                    $form.find('input[name="Content-Type"]').val(file.type);
 
-                var $key = $form.find('input[name="key"]');
-                var newKeyVal = $key.val().replace(/^([a-zA-Z0-9]+\/).*/, '$1' + file.name);
-                $key.val(newKeyVal);
+                    var $key = $form.find('input[name="key"]');
+                    var newKeyVal = $key.val().replace(/^([a-zA-Z0-9]+\/).*/, '$1' + file.name);
+                    $key.val(newKeyVal);
 
-                var $progress = $('<div/>', {
-                    class: 'progress',
-                    'data-mod': file.size,
-                    'data-file': file.name
+                    var $progress = $('<div/>', {
+                        class: 'progress',
+                        'data-mod': file.size,
+                        'data-file': file.name
+                    });
+
+                    var $bar = $('<div/>', {
+                        class: 'bar',
+                        'data-file': file.name
+                    });
+                    $bar.css('width', '0%');
+                    $progress.append($bar);
+
+                    $('.progress-bar-area').append($progress);
+                    $progress.slideDown('slow');
+
+                    data.submit();
                 });
-
-                var $bar = $('<div/>', {
-                    class: 'bar',
-                    'data-file': file.name
-                });
-                $bar.css('width', '0%');
-                $progress.append($bar);
-
-                $('.progress-bar-area').append($progress);
-                $progress.slideDown('slow');
-
-                data.submit();
             });
         },
 
@@ -63,11 +95,18 @@ $(function () {
 
                 initHideAble($('.progress[data-file="' + file.name + '"]'));
             });
+
+            uploading--;
+            updateUploadQueue(uploadQueue -= data.files.length);
         },
 
         done: function (event, data) {
             data.files.forEach(function (file) {
-                $('.progress[data-file="' + file.name + '"]').slideUp('slow');
+                var $progress = $('.progress[data-file="' + file.name + '"]');
+                $progress.slideUp('slow', function () {
+                    $progress.remove();
+                });
+
 
                 var $fileList = $('#file-list');
                 if ($fileList.find('a:contains("' + file.name + '")').length !== 0)
@@ -88,6 +127,9 @@ $(function () {
                 //noinspection NodeModulesDependencies
                 $.get(fileUploadedUrl);
             });
+
+            uploading--;
+            updateUploadQueue(uploadQueue -= data.files.length);
         }
     });
 });
